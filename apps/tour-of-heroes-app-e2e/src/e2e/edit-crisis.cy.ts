cb742935cef19d93374c6d7661a0f4697c61bbb9
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-10-23 23:25:03 +0200

    test: correct query and test steps

--- apps/tour-of-heroes-app-e2e/src/e2e/edit-crisis.cy.ts
+++ apps/tour-of-heroes-app-e2e/src/e2e/edit-crisis.cy.ts
@@ -1,20 +1,30 @@
 const getCrisisCenterHomeGreeting = () =>
-  cy.contains('Welcome to the Crisis Center');
-const getCrisisLink = (name: string) => cy.contains(name, 'a');
-const getNameControl = () => cy.get('[placholder=name]');
-const getSaveButton = () => cy.contains('Save', 'button');
-const getSelectedCrisis = (name: string) => cy.contains(name, '.selected a');
+  cy.contains('Welcome to the Crisis Center', {
+    matchCase: false,
+  });
+const getCrisisLink = (name: string) => cy.get('a').contains(name, {
+  matchCase: false,
+});
+const getNameControl = () => cy.get('[placeholder=name]');
+const getSaveButton = () => cy.get('button').contains('Save', {
+  matchCase: false,
+});
+const getSelectedCrisis = (name: string) =>
+  cy.get('.selected a').contains(name, {
+    matchCase: false,
+  });
 
 it('Edit crisis from crisis detail', () => {
   const crisisId = 2;
   const newCrisisName = 'The global temperature is rising';
   cy.visit(`/crisis-center/${crisisId}`);
 
-  getNameControl().clear().type(newCrisisName).click();
+  getNameControl().clear().type(newCrisisName);
+  getSaveButton().click();
+
   getSelectedCrisis(newCrisisName).should('be.visible');
   cy.location().should((location) => {
-    expect(location.pathname).to.equal(`/crisis-center`);
-    expect(location.href).to.match(new RegExp(`;id=${crisisId};foo=foo$`));
+    expect(location.pathname).to.equal(`/crisis-center;id=${crisisId};foo=foo`);
   });
 });
 
